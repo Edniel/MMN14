1c5ec0bf3d5a24b0bf8a7c3916ed22466771d53d
Edniel <77395047+Edniel@users.noreply.github.com>
2021-03-31 18:48:51 +0300

    Update Ex14.java

--- Ex14.java
+++ Ex14.java
@@ -7,27 +7,6 @@
 public class Ex14
 {
     public static void Main(){
-        //First task
-        /*
-        int[] a1={5,21,3,22,12,7,27,6,4};
-        System.out.println(maximalDrop(a1));//23
-        int[] a2={5,21,3,22,12,7,26,14};
-        System.out.println(maximalDrop(a2));//18
-        int[] a3={5,15,3,22,12,7,27,14};
-        System.out.println(maximalDrop(a3));//15
-         */
-
-        //Second task
-        /*
-        int[][] exA={{0,1,0,1,1,0},{1,0,1,1,0,0},{0,0,0,1,0,1},{0,0,0,0,0,0},{1,0,1,1,0,0},{0,1,0,1,1,1}};
-        print(exA);
-        System.out.println("************");
-        int[][] exB={{0,1,0,0,0,1},{1,0,0,1,1,1},{0,0,0,0,0,0},{1,1,1,1,1,1},{0,1,0,1,0,1},{1,0,0,0,1,0}};
-        print(exB);
-        System.out.println(isSink(exA));
-        System.out.println(isSink(exB));
-         */
-
         //Third task
         boolean[][] matA={{false,false,false},{false,true,false},{false,true,false},{false,false,false}};
         boolean[][] matB={{false,true,false,false,true},{true,false,false,true,true},{false,false,true,true,false,},

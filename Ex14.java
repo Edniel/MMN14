afea079e9e403c30cec48c800b95664eb160cb95
Edniel <77395047+Edniel@users.noreply.github.com>
2021-04-08 16:32:43 +0300

    Update Ex14.java

--- Ex14.java
+++ Ex14.java
@@ -6,24 +6,8 @@
  */
 public class Ex14
 {
-    public static void Main(){
-        //Third task
-        boolean[][] matA={{false,false,false},{false,true,false},{false,true,false},{false,false,false}};
-        boolean[][] matB={{false,true,false,false,true},{true,false,false,true,true},{false,false,true,true,false,},
-                {true,false,false,false,false},{true,true,true,false,false}};
-        //print(matB);
-        //System.out.println(size(matB,2,2));
-        //System.out.println(size(matB,0,1));
-        //System.out.println(size(matB,1,4));
-        //size(matB,1,3);
-        size0(matB,4,1);
-        size0(matB,4,0);
-        size0(matB,4,2);
-        size0(matB,3,0);
-    }
-
-    /**
-     * This method returns the highest drop between two numbers.The low number must appear after the high number.
+   /**
+     * This method returns the highest drop between two numbers.The high number must appear first.
      * @param a an int array for sorting the highest drop
      * @return an int represeting the highest "drop" (natural numbers differance).
      * with a complexity of O(n).
@@ -49,16 +33,31 @@ public class Ex14
      * @return
      */
     public static int isSink(int[][] mat){
+        int[][] measures=measure(mat);
+        return checkMeasures(measures,0);
+    }
+
+    private static int[][] measure(int[][] mat){
         int[][] measures=new int[2][mat.length];
-        for(int x=0;x<mat.length;x++)
-            for(int y=0;y<mat[0].length;y++){
-                measures[0][x]+=mat[x][y];
-                measures[1][y]+=mat[x][y];
-            }
-        for(int temp=0;temp<mat.length;temp++)
-            if(measures[0][temp]==0 && measures[1][temp]==mat.length-1)
-                return temp; 
-        return -1;
+        return measure(mat,0,0,measures);
+    }
+
+    private static int[][] measure(int[][] mat,int x,int y,int[][]measures){
+        if(y==mat.length)
+            return measures;
+        if(x==mat.length)
+            return measure(mat,0,y+1,measures);
+        measures[0][x]+=mat[x][y];
+        measures[1][y]+=mat[x][y];
+        return measure(mat,x+1,y,measures);
+    }
+
+    private static int checkMeasures(int[][]measures,int temp){
+        if(temp==measures[0].length)
+            return -1;
+        if(measures[0][temp]==0 && measures[1][temp]==measures[0].length-1)
+            return temp;
+        return checkMeasures(measures,temp+1);
     }
 public static int size(boolean[][] mat,int x,int y){
         if(x<0 || x>=mat.length || y<0 || y>=mat[0].length || mat[x][y]!=true) 
@@ -103,77 +102,30 @@ public static int size(boolean[][] mat,int x,int y){
      * This method
      * @param
      * @return
-     */
-    private static void print(int[][] a){
-        for(int x=0;x<a.length;x++){
-            if(x!=0)System.out.println();
-            for(int y=0;y<a[0].length;y++)
-                System.out.print(a[x][y]+" ");
-        }
-        System.out.println();
-    }
-
-    private static void print(boolean[][] a){
-        for(int x=0;x<a.length;x++){
-            if(x!=0)System.out.println();
-            for(int y=0;y<a[0].length;y++)
-                System.out.print(a[x][y]+" ");
-        }
-        System.out.println();
-    }
-
-    public static int size1(boolean[][] mat,int x,int y){
-        if(x<0 || y<0 || x>=mat.length|| y>=mat[0].length|| mat[x][y]==false)
-            return 0;
-        int temp=1;
-        int[][] sinks=new int[2][mat.length*mat[0].length];
-        sinks[0][0]=x;
-        sinks[1][0]=y;
-        temp=size1(mat,x,y,sinks,temp);
-        return temp;
-    }
-
-    private static int size1(boolean[][] mat,int x,int y,int[][] sinks,int temp){
-        if(x<mat.length && y<mat[0].length && mat[x+1][y+1]==true)
-            if(checkPoint(sinks,x+1,y+1)){
-                sinks[0][temp]=x;
-                sinks[1][temp]=y;
-                size1(mat,x,y,sinks,++temp);
-            }
-
-        return temp;
-    }
-
-    private static boolean checkPoint(int[][] sinks,int x,int y){
-        for(int t=0;t<sinks[0].length;t++){
-            if(sinks[0][t]==x && sinks[1][t]==y)
-                return false;
-        }
-        return true;
-    }
-    
-    
-    
-    
-    
-    
-    
-    
+     */ 
    public static boolean isPermutation(int[] a,int[] b){
-     return isPermutation(a,b,0,0);  
-   }
-   private static boolean isPermutation(int[] a,int[] b,int ind,int temp){
-       if(ind==a.length)
-           return true;
-       if(temp==b.length)
-           return false;
-       if(a[ind]==b[temp])
-           return isPermutation(a,b,ind+1,0);
-       return isPermutation(a,b,ind,temp+1);
-       
-   }
-    
-    
-    
-    
+        if(a.length != b.length || calculateSum(a)!=calculateSum(b)) 
+            return false;
+        if(isPermutation(a,b,0,0))
+            return true;
+        return false;
+    }
+    private static int calculateSum(int[] arr){
+        return calculateSum(arr,0,0);
+    }
+
+    private static int calculateSum(int[] arr,int temp,int sum){
+        if(temp==arr.length)
+            return sum;
+        return calculateSum(arr,temp+1,sum+arr[temp]);
+    }
+    private static boolean isPermutation(int[] a,int[] b,int ind,int temp){
+        if(ind==a.length)
+            return true;
+        if(temp==b.length)
+            return false;
+        if(a[ind]==b[temp])
+            return isPermutation(a,b,ind+1,0);
+        return isPermutation(a,b,ind,temp+1);
+    }
 }
